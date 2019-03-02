4cdf4a1733ca6c62923b4915e3d9b55a4ce5fbc1
Sagar Shrestha <sg28r8@gmail.com>
2019-03-02 00:34:35 -0500

    Enemy added

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pyglet as pig
 from pyglet.window import key
 from player import *
 from pyglet.sprite import Sprite
+from random import randint
+import time
+
+ 
 class game(pig.window.Window):
     def __init__(self, *args,**kwargs):
         super().__init__(*args,**kwargs)# getting the constructor for the windows  
@@ -13,18 +17,23 @@ class game(pig.window.Window):
         self.attack = load_files('attack.gif')
         self.melee = load_files('melee.gif')
         self.bullet = load_files('bullet.png')
+        self.enemy = Sprite(load_files('run.gif'))
         self.player = object((self.height/2),(self.width/2),Sprite(self.move))
         self.player_x = self.player.x_position
         self.player_y = self.player.y_postion
+        self.enemy.image.anchor_x = 64/2
+        self.enemy.image.anchor_y = 64/2
+        self.enemy.rotation = 180.0
+
+        
         self.bullet_list = []
         self.map_List = []
-        self.enemy_list = []
-        
-        
+        self.enemy_list = []      
         for i in range(4):
             self.map_List.append(object(0,i*960, Sprite(self.bg)))
         for tile in self.map_List:
             tile.acceration = -200
+    
     def on_key_press(self,symbol, modifiers):
 
         if symbol == key.RIGHT:
@@ -49,23 +58,43 @@ class game(pig.window.Window):
             self.player.acceration = 0
         if symbol in (key.SPACE,key.M):
             self.player = object(self.player_x,self.player_y,Sprite(self.move))
-    #def get_bounds(self):
+    
 
     def on_draw(self):
-        self.clear()      
+        self.clear()
+          
         for tile in self.map_List:
             tile.draw()
         self.player.draw()
+        for enemy in self.enemy_list:
+            enemy.draw()
+            self.y_pos = enemy.y_postion
+            print(self.y_pos)
+
+
         for bullet in self.bullet_list:
             bullet.draw()
             self.y_pos = bullet.y_postion
             print(self.y_pos)
+
+    def make_enemy(self):
+        selector = randint(0,10)
+        if selector == 1:
+            self.enemy_list.append(object(randint(100,500),1000,self.enemy))      
+       
     def bullet_update(self,dt):
         for bullet in self.bullet_list:
             bullet.update(dt)
-            bullet.acceration = 300 
-
-
+            bullet.acceration = 300
+            if bullet.y_postion > 1000:
+                self.bullet_list.remove(bullet)
+                
+    def enemy_update(self,dt):
+        for enemy in self.enemy_list:
+            enemy.update(dt)
+            enemy.acceration = -400
+            if enemy.y_postion <-1000:
+                self.enemy_list.remove(enemy)                    
     def map_update(self,dt):
         for tile in self.map_List:
             tile.update(dt)
@@ -75,10 +104,18 @@ class game(pig.window.Window):
             tile.acceration = -200
     def update(self,dt):
         #print('X-position:',self.player.x_position,'Y-position',self.player.y_postion)
-        print(self.bullet_list)
+        print(self.enemy_list)
+  
+        
         self.player.update(dt)
         self.map_update(dt)
         self.bullet_update(dt)
+        self.enemy_update(dt)
+        if(len(self.enemy_list)<10):
+            self.make_enemy()
+        
+        
+
         self.player_x = self.player.x_position
         self.player_y = self.player.y_postion
 
